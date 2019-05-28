b722c6573f7320341c299116735225782cd81abd
gabsmoreira <gabrielfm2@al.insper.edu.br>
2019-05-28 12:58:36 -0300

    testcuda

--- test.cu
+++ test.cu
@@ -1,20 +1,27 @@
 #include <iostream>
 
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
+
 
 __global__ void kernel_function(float *pixel, int lenX, int lenY){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int index;
-    if(i < lenX && j < lenY){
-        index = i*3 + j*lenX*3;
-        pixel[index + 0] = float(i) / lenX;
-        pixel[index + 1] = float(j) / lenY;
-        pixel[index + 2] = 0.2;
-    }
-    else{
-        //out of bounds
-        return;
-    }
+    if((i >= max_x) || (j >= max_y)) return;
+    index = i*3 + j*lenX*3;
+    pixel[index + 0] = float(i) / lenX;
+    pixel[index + 1] = float(j) / lenY;
+    pixel[index + 2] = 0.2;
 }
 
 int main() {
@@ -24,7 +31,7 @@ int main() {
     size_t size_pixels = 3*num_pixels*sizeof(float);
     
     float *pixel;
-    cudaMallocManaged((void **)&pixel, size_pixels);
+    checkCudaErrors(cudaMallocManaged((void **)&pixel, size_pixels));
 
     // definindo o tamanho do bloco
     dim3 blocks(nx/8+1,ny/8+1);
@@ -32,6 +39,8 @@ int main() {
     dim3 threads(8,8);
     // chamando o kernel
     kernel_function<<<blocks, threads>>>(pixel, nx, ny);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     
     for (int j = ny-1; j >= 0; j--) {
@@ -46,6 +55,7 @@ int main() {
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+    checkCudaErrors(cudaFree(pixel));
 }
 
 
