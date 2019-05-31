7be4ec308a886f8e207f96c10f578d55eb8795ca
gabsmoreira <gabrielfm2@al.insper.edu.br>
2019-05-31 09:24:38 -0300

    errors

--- main.cu
+++ main.cu
@@ -23,7 +23,7 @@ __global__ void kernel_function(float *pixels, int lenX, int lenY, hitable *worl
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
-    printf("%d\n", list[0].radius);
+    // printf("%d\n", list[0]);
     if((i >= lenX) || (j >= lenY)) return;
 
     float u = float(i) / float(lenX);
