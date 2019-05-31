cdf8f131d81611dc8625d9a055988f5a5a5638a0
gabsmoreira <gabrielfm2@al.insper.edu.br>
2019-05-31 09:08:32 -0300

    errors

--- main.cu
+++ main.cu
@@ -39,7 +39,7 @@ __global__ void kernel_function(float *pixels, int lenX, int lenY, hitable *worl
 __global__ void kernel_init(hitable *list, hitable *world){
     list[0] = new sphere(vec3(0,0,-1), 0.5);
     list[1] = new sphere(vec3(0,-100.5,-1), 100);
-    *world = new hitable_list(list,2);
+    world = new hitable_list(list,2);
 }
 
 int main() {
@@ -52,8 +52,8 @@ int main() {
     
     float *pixels;
     float *pixelsCPU;
-    hitable **list[2];
-    hitable **world;
+    hitable *list;
+    hitable *world;
     cudaMallocManaged((void **)&pixels, size_pixels);
     cudaMallocManaged((void **)&list, size_list);
     cudaMallocManaged((void **)&world, size_world);
